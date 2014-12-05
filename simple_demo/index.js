4b201bb3191ad16eb000e079573f904a529bb925
金健翔 <andy_ios@163.com>
2014-12-05 13:12:03 +0800

    完成简单测试

--- simple_demo/index.js
+++ simple_demo/index.js
@@ -25,24 +25,26 @@ function build_exec(pro_path, apk_output_path, keystore_path, keystore_name, key
 	var src_path = pro_path + '/src';
 
 	// 设置输出缓存文件路径
-	var class_path   = pro_path + '/cache/classes';
-	var dx_path      = pro_path + '/cache/dx';
-	var unsigned_apk_path     = pro_path + '/cahce/unsigned.apk';
-	var package_path = pro_path + '/cache/resources-package';
+	var class_path         = pro_path + '/cache/classes';
+	var dx_path            = pro_path + '/cache/dx';
+	var package_path       = pro_path + '/cache/resources-package';
+	var unsigned_apk_path  = pro_path + '/unsigned.apk';
 
 	// 移除缓存
 	shell.exec('rm -rf ' + pro_path + '/cache');
+	shell.exec('rm -rf ' + pro_path + '/*.apk');
 
 	// 初始化文件目录
 	shell.exec('mkdir -p ' + class_path);
-	shell.exec('mkdir -p'  + dx_path);
+	shell.exec('mkdir -p '  + dx_path);
+
 
 	// 开始执行脚本
 	_exec.aadp_exec(gen_path, res_path, xml_path, SDK_ANDROID_JAR_PATH);
-	_exec.compile_exec(pro_path, class_path, SDK_ANDROID_JAR_PATH, libs, SDK_MAP_JAR_PATH);       
-	_exec.dex_exec(dx_path, class_path, libs, SDK_MAP_JAR_PATH);
-	_exec.package_exec(xml_path, res_path, ass_path, SDK_SDKLIB_JAR_PATH, package_path);
+	_exec.compile_exec(pro_path, class_path, SDK_ANDROID_JAR_PATH, lib_path, SDK_MAP_JAR_PATH);       
+	_exec.dex_exec(dx_path, class_path, lib_path, SDK_MAP_JAR_PATH);
+	_exec.package_exec(xml_path, res_path, ass_path, SDK_ANDROID_JAR_PATH, package_path);
 	_exec.apk_builder_exec(SDK_SDKLIB_JAR_PATH, unsigned_apk_path, package_path, dx_path + '/*.dex', src_path, lib_path);
 	_exec.jarsigner_exec(keystore_path, keystore_pas, keystore_name, apk_output_path, unsigned_apk_path);
 }
-
+build_exec('.', 'aa.apk', '/Users/andy/Desktop/android.keystore', 'android.keystore', 'nationsky88');
